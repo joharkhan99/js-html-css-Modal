f1d555f2d0b7ee5c3b28b7de1c5f466c66eee4ee
single-byte <70197335+single-byte@users.noreply.github.com>
2020-11-09 00:49:44 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+document.getElementById('btn').addEventListener('click', () => {
+  let elem = document.getElementById('modal');
+
+  if (elem.style.display == 'none') {
+    elem.style.display = 'block';
+    document.get
+  } else {
+    elem.style.display = 'none';
+  }
+});
+
+
+document.querySelector('.cross').addEventListener('click', ()=>{
+  let elem = document.getElementById('modal');
+
+  if (elem.style.display == 'none') {
+    elem.style.display = 'block';
+  } else {
+    elem.style.display = 'none';
+  }
+});
